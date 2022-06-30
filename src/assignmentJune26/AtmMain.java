8c8e34e0f02e329237e54fddb625d7238c48a2a9
albinjoseph5 <ajalbinjoseph@gmail.com>
2022-06-29 21:04:05 -0400

    Assignment for June 26

--- src/assignmentJune26/AtmMain.java
+++ src/assignmentJune26/AtmMain.java
@@ -8,6 +8,7 @@ public class AtmMain {
 		int pinNumber;
 		long accountNumber;
 		int option;
+		String flag = "y";
 
 		Person person1 = new Person("Albin", 987654, 1000, 1234);
 		Person person2 = new Person("Jithin", 123456, 3000, 4321);
@@ -21,23 +22,27 @@ public class AtmMain {
 		System.out.println("Enter the PIN number : ");
 		pinNumber = in.nextInt();
 		Person returnedPerson = person1.validatePin(person, pinNumber, accountNumber);
-		if (returnedPerson != null) {
-			operation.homePage();
-			option = in.nextInt();
-			switch (option) {
-			case 1:
-				operation.depositCash(returnedPerson);
-				break;
-			case 2:
-				operation.withdrawCash(returnedPerson);
-				break;
-			case 3:
-				operation.currentBalance(returnedPerson);
-				break;
-			default:
-				System.out.println("Invalid option!!!");
-				break;
+		while (flag.equals("y")) {
+			if (returnedPerson != null) {
+				operation.homePage();
+				option = in.nextInt();
+				switch (option) {
+				case 1:
+					operation.depositCash(returnedPerson);
+					break;
+				case 2:
+					operation.withdrawCash(returnedPerson);
+					break;
+				case 3:
+					operation.currentBalance(returnedPerson);
+					break;
+				default:
+					System.out.println("Invalid option!!!");
+					break;
+				}
 			}
+			System.out.println("Would like to do another transaction (y/n)?");
+			flag = in.next();
 		}
 		in.close();
 	}
